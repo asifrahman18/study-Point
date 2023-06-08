923a41fd70921fdc976d161eaef51b31354d213e
Asif Rahman <57532856+asifrahman18@users.noreply.github.com>
2023-06-08 21:39:04 +0600

    Update enrolled.php

--- enrolled.php
+++ enrolled.php
@@ -103,13 +103,13 @@ include('connect.php');
 
                             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                 <ul class="navbar-nav ml-auto">
-                                    < class="nav-item active">
+                                    <li class="nav-item active">
                                         <a class="nav-link" href="adminProfile.php">Home <span
                                                 class="sr-only">(current)</span></a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a class="nav-link" id="s-out" href="index.php">Sign Out</a>
-                                        </li>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" id="s-out" href="index.php">Sign Out</a>
+                                    </li>
                                 </ul>
                             </div>
                         </nav>
